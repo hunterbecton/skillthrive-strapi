4028f53a743304f46ea89d8e79736555331eb396
hunterbecton <hunter@hunterbecton.com>
2018-11-05 21:49:26 -0500

    changed api url

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,9 +14,10 @@ module.exports = {
     {
       resolve: 'gatsby-source-strapi',
       options: {
-        apiURL: 'http://localhost:1337',
+        apiURL: 'https://skillthrive-api.herokuapp.com',
         contentTypes: [ // List of the Content Types you want to be able to request from Gatsby.
           'project',
+          'lesson',
           'user'
         ]
       },
